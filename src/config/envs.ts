ae3b6c886211f2b26d8259069504f934bdafdafe
maycolroa <lehallo@hotmail.com>
2024-07-25 15:17:23 -0500

    add proyect backend  0.00 25/07/2014

---
+++ src/config/envs.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { get } from 'env-var';
+
+
+export const envs = {
+
+  PORT: get('PORT').required().asPortNumber(),
+
+// MongoDB
+  MONGO_URL: get('MONGO_URL').required().asString(),
+  MONGO_DB_NAME: get('MONGO_DB').required().asString(),
+  MONGO_USER: get('MONGO_USER').required().asString(),
+  MONGO_PASSWORD: get('MONGO_PASSWORD').required().asString(),
+
+  //postgres
+  POSTGRES_URL: get('POSTGRES_URL').required().asString(),
+  POSTGRES_DB_NAME: get('POSTGRES_DB').required().asString(),
+  POSTGRES_USER: get('POSTGRES_USER').required().asString(),
+  POSTGRES_PASSWORD: get('POSTGRES_PASSWORD').required().asString(),
+}

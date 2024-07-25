4ee1519945d852e47c20860933a7769e6056682a
maycolroa <lehallo@hotmail.com>
2024-07-25 15:26:23 -0500

    envs correction MONGO:DB_USER  0.01 25/07/2014

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -8,7 +8,7 @@ export const envs = {
 
 // MongoDB
   MONGO_URL: get('MONGO_URL').required().asString(),
-  MONGO_DB_NAME: get('MONGO_DB').required().asString(),
+  MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
   MONGO_USER: get('MONGO_USER').required().asString(),
   MONGO_PASSWORD: get('MONGO_PASSWORD').required().asString(),
 
